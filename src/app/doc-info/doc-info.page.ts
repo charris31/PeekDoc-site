696fc7f77824770b962a65e51194284e33a7f6ff
TXT-44 <txt-44@TXT-44.local>
2021-07-20 14:36:43 -0700

    The commit before inputing the true map location feature

--- src/app/doc-info/doc-info.page.ts
+++ src/app/doc-info/doc-info.page.ts
@@ -7,12 +7,29 @@ declare var google;
   styleUrls: ['./doc-info.page.scss'],
 })
 export class DocInfoPage implements OnInit {
+  doctor = {
+    name: 'John Doe',
+    gender: 'Male',
+    race: 'African American',
+    birth: 'Los Angeles, CA',
+    school: 'Charles Drew University',
+    specialties: 'Physician',
+    hobbies: 'Basketball, Cooking, Traveling',
+    phoneNumber: '(123) 456-7890',
+    webPage: 'www.madeupwebsite.com',
+    media: "DrJohnDoe"
+  };
 
-  constructor() { }
+  business = {
+    location: '12736 S Avalon Blvd, Los Angeles CA 90061'
+    
+  }
+  
+
+  constructor() { 
+  }
 
   ngOnInit() {
   }
 
 }
-
-
