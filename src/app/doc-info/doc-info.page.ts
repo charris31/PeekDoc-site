96b4895f5219b62ea74434fac0ddc7ce3c766895
TXT-44 <txt-44@TXT-44.local>
2021-07-21 13:55:24 -0700

    The commit AFTER inputing the true map location feature

--- src/app/doc-info/doc-info.page.ts
+++ src/app/doc-info/doc-info.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-declare var google;
+import { ViewChild, ElementRef } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-doc-info',
@@ -25,8 +27,72 @@ export class DocInfoPage implements OnInit {
     
   }
   
+  map: any;
 
-  constructor() { 
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
+  infoWindows: any = [];
+  markers: any = [
+    {
+      title: "Imaginary Clinic",
+      latitude: "33.91696109173674",
+      longitude: "-118.26476780243469"
+    }
+  ]
+
+  constructor() {}
+
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  addMarkersToMap(markers) {
+    for (let marker of markers) {
+      let position = new google.map.LatLng(marker.latitude, marker.longitude);
+      let mapMarker = new google.map.Marker({
+        position: position,
+        title: marker.title,
+        latitude: marker.latitude,
+        longitude: marker.longitude
+      });
+
+      mapMarker.setMap(this.map);
+      this.addInfoWindowToMarker(mapMarker);
+    }
+  }
+
+  addInfoWindowToMarker(marker) {
+    let infoWindowContent = '<div id="content">' +
+                              '<h2 id="firstHeading" class="firstHeading">' + marker.title + '</h2>'
+                              '<p>Latitude: ' + marker.latitude + '</p>' +
+                              '<p>Longitude: ' + marker.longitude + '</p>' +
+                            '</div>';
+    let infoWindow = new google.maps.infoWindow({
+      content: infoWindowContent
+    });
+
+    marker.addListener('click', () =>{
+      this.closeAllInfoWindows();
+      infoWindow.open(this.map, marker);
+    });
+    this.infoWindows.push(infoWindow);
+  }
+
+  closeAllInfoWindows() {
+    for(let window of this.infoWindows) {
+      window.close();
+    }
+  }
+
+  showMap () {
+    const location = new google.maps.LatLng(33.91696109173674, -118.26476780243469);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+    this.addMarkersToMap(this.markers);
   }
 
   ngOnInit() {
--- src/app/filter/filter.page.ts
+++ src/app/filter/filter.page.ts
@@ -14,9 +14,12 @@ export class FilterPage implements OnInit {
   constructor(public navCtrl: NavController) { 
 
     this.originalData = [
-      {name: 'John Doe', specialty: 'Physician', avatar: 'https://bit.ly/36Cy6CO', about: 'I am a good doctor'},
-      {name: 'Jane Doe', specialty: 'Surgeon', avatar: 'https://bit.ly/36GxFaG', about: 'I am also a good doctor'}
+      {name: 'John Doe', specialty: 'Physician', pic: 'https://bit.ly/36Cy6CO', about: 'I am a good doctor'},
+      {name: 'Jane Doe', specialty: 'Dentist', pic: 'https://bit.ly/36GxFaG', about: 'I am also a good doctor'},
+      {name: 'Silas Stone', specialty: 'Surgeon/Scientist', pic: 'https://bit.ly/36GxFaG', about: 'I work at S.T.A.R. Labs'},
+      {name: 'Harleen Quinzel', specialty: 'Psychiatrist', pic: 'https://bit.ly/36GxFaG', about: 'There is always a wild side to an innocent face'}
     ];
+
     this.modifiedData = JSON.parse(JSON.stringify(this.originalData));
   }
 
