314f86e01da70f31a8bc0ada2437bbca8a38998f
TXT-44 <txt-44@TXT-44.local>
2021-07-24 02:39:13 -0700

    Commit after publishing the second MVP demo

--- src/app/filter/filter.page.ts
+++ src/app/filter/filter.page.ts
@@ -10,10 +10,10 @@ export class FilterPage implements OnInit {
 
   searchTerm: string;
   doctors = [
-    {name: 'John Doe', specialty: 'Physician', pic: 'https://bit.ly/36qMUof', about: 'I am a good doctor'},
-    {name: 'Jane Doe', specialty: 'Dentist', pic: 'https://bit.ly/3zokxmU', about: 'I am also a good doctor'},
-    {name: 'Silas Stone', specialty: 'Surgeon/Scientist', pic: 'https://bit.ly/3iulbsi', about: 'I work at S.T.A.R. Labs'},
-    {name: 'Harleen Quinzel', specialty: 'Psychiatrist', pic: 'https://bit.ly/3eIhk9S', about: 'There is always a wild side to an innocent face'}
+    {name: 'John Doe', specialty: 'Physician', pic: 'https://bit.ly/3kLZxm9', about: 'I am a good doctor'},
+    {name: 'Jane Doe', specialty: 'Dentist', pic: 'https://bit.ly/3iKi7Iy', about: 'I am also a good doctor'},
+    {name: 'Andrew Rue', specialty: 'Surgeon/Scientist', pic: 'https://bit.ly/3x7FHEA', about: 'I am the right doctor for you'},
+    {name: 'Sofia Garcia', specialty: 'Psychiatrist', pic: 'https://bit.ly/3ePLTue', about: 'I can be a great doctor for you too'}
   ];
 
   originalData: any;
@@ -22,7 +22,7 @@ export class FilterPage implements OnInit {
   constructor(public navCtrl: NavController) { 
 
   // I made this way too complicated
-  
+
   //   this.originalData = [
   //     {name: 'John Doe', specialty: 'Physician', pic: 'https://bit.ly/36qMUof', about: 'I am a good doctor'},
   //     {name: 'Jane Doe', specialty: 'Dentist', pic: 'https://bit.ly/3zokxmU', about: 'I am also a good doctor'},
