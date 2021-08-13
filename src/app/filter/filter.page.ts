d32edcc00533d697e4e148343cf87ecc4eddf2ec
Michael Sezen <michaelsezen@Michaels-MacBook-Pro-2.local>
2021-08-13 14:47:19 -0400

    final demo

--- src/app/filter/filter.page.ts
+++ src/app/filter/filter.page.ts
@@ -12,7 +12,7 @@ import { PopOverService } from '../services/popOver/pop-over.service';
 })
 export class FilterPage implements OnInit {
 
-  // This is for the specialties 
+  // This is for the specialties
   isPhysician: boolean = false;
   isDentist: boolean = false;
   isSurgeon: boolean = false;
@@ -27,7 +27,7 @@ export class FilterPage implements OnInit {
   searchTerm: string;
   selected_option: string;
   doctors = [
-    {name: 'John Doe', specialty: 'Physician', pic: 'https://bit.ly/3kLZxm9', race: 'Black/African American', gender: 'Male', language: 'English', insurance: 'Any'},
+    {name: 'John Doe', specialty: 'Physician', pic: 'https://bit.ly/3kLZxm9', race: 'Black', gender: 'Male', language: 'English', insurance: 'Any'},
     {name: 'Jane Doe', specialty: 'Dentist', pic: 'https://bit.ly/3kVdrCx', race: 'Asian', gender: 'Female', language: 'English, Korean', insurance: 'Any'},
     {name: 'Andrew Rue', specialty: 'Surgeon', pic: 'https://bit.ly/3x7FHEA', race: 'Middle Eastern', gender: 'Male', language: 'English', insurance: 'Any'},
     {name: 'Sofia Garcia', specialty: 'Psychiatrist', pic: 'https://bit.ly/3ePLTue', race: 'Latin', gender: 'Female', language: 'English, Spanish', insurance: 'Any'}
@@ -41,7 +41,7 @@ export class FilterPage implements OnInit {
 
   constructor(public navCtrl: NavController, private popCtrl: PopoverController, private service: PopOverService, private activatedRoute: ActivatedRoute) {
 
-  
+
   }
 
 
@@ -70,7 +70,7 @@ export class FilterPage implements OnInit {
     console.log(this.filterArr);
 
     let res = this.doctors.filter( (doctor)=> {
-      return (this.filterArr.indexOf(doctor.specialty) >=0);  
+      return (this.filterArr.indexOf(doctor.specialty) >=0);
     })
     this.doctors = res;
   }
@@ -96,5 +96,5 @@ export class FilterPage implements OnInit {
   psychiatristToggle(){
     this.isPsychiatrist = true;
   }
-  
+
 }
